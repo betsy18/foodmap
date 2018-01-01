a0d94a0fbaeebc10a6a92c7bbd71b85d3890bf3d
betsy18 <betsy_dessire@hotmail.com>
2017-12-31 21:53:48 -0500

    Implementando el obj data.js

---
+++ js/app.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+  setTimeout(function() {
+    window.location.href = 'views/menu.html';
+  }, 5000);
+});
---
+++ js/data.js
@@ -0,0 +1,20 @@
+var restaurant = {
+  'La Tranquera BBQ': {
+    'direccion': 'Av. José Pardo 285',
+    'plato1': ['bbq', '45.90', 'assets/images/bbq.png'],
+    'plato2': ['costillas', '52.00', 'assets/images/costillas.png'],
+    'plato3': ['vapor', '49.90', 'assets/images/vapor.png']
+  },
+  'El 10 carnes y vinos': {
+    'direccion': 'Av. Petit Thouars 4599',
+    'plato1': ['ensalada', '45.90', 'assets/images/ensalada-atun.png'],
+    'plato2': ['pure', '52.00', 'assets/images/pure.png'],
+    'plato3': ['meat', '49.90', 'assets/images/meat-1.png']
+  },
+  'Baco y Vaca': {
+    'direccion': 'Av. Dos de Mayo 798',
+    'plato1': ['lomo', '45.90', 'assets/images/lomo.png'],
+    'plato2': ['pachamanca', '52.00', 'assets/images/pachamanca.png'],
+    'plato3': ['salchipapa', '49.90', 'assets/images/salchipapa.png']
+  }
+};
